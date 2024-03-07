033a4c92750b2c7c4aefda0e08c5a7f2ca6f8ab0
David <Marin9737@gmail.com>
2024-03-07 18:00:04 -0500

    for  spencer to look at while im on my way home

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import Inputs from './Inputs';
 import TimeAndLocation from './TimeAndLocation';
 import TemperatureAndDetails from './TemperatureAndDetails';
 import Forecast from './Forecast';
-import getWeatherData from './Weather';
-
+import getFormattedWeatherData from './Weather';
+import {DateTime} from 'luxon'
 
 
 function App() {
   const fetchWeather = async () =>{
-    const data = await getWeatherData("weather",{ q: "brooklyn"});
+    const data = await getFormattedWeatherData({ q: "New york"});
     console.log(data)
-  }
-  fetchWeather()
+  };
+
+  fetchWeather();
   return (
     <div
     className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400"
